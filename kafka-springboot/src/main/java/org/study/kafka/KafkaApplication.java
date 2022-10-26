04e5d51b65aebedad506bbb0604bce59d8dfce05
yandi <prozhw@qq.com>
2022-10-26 09:50:15 +0800

    kafka springboot

--- kafka-springboot/src/main/java/org/study/kafka/KafkaApplication.java
+++ kafka-springboot/src/main/java/org/study/kafka/KafkaApplication.java
@@ -1,4 +1,11 @@
 package org.study.kafka;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class KafkaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
 }
